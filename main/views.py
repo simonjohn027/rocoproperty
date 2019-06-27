123f700278244c33c35195200a59333af7610205
simonjohn027 <simonjohn027@gmail.com>
2019-06-28 01:57:28 +0400

    Mapping Page

--- main/views.py
+++ main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'main/index.html')
+
+def propertyList(request):
+    return  render(request, 'main/properties.html')
