ae2d7b3371cc6dff7d0a1361f08f6d249f1ba590
simonjohn027 <simonjohn027@gmail.com>
2019-06-29 15:58:03 +0400

    Adding Contact Pages

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from . import  views
 
 urlpatterns = [
     path("", views.index, name = "index"),
-    path("props",views.propertyList, name = "list")
+    path("props",views.propertyList, name = "list"),
+    path("prop",views.property, name = "property"),
+    path("contact",views.contact, name = "contact")
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,9 @@ def index(request):
 
 def propertyList(request):
     return  render(request, 'main/properties.html')
+
+def property(request):
+    return  render(request, 'main/properties-detail.html')
+
+def contact(request):
+    return  render(request, 'main/contact.html')
